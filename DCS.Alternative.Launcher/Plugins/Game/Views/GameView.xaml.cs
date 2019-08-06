429e3169106fd4f5a40a10ebff5ce507dc4e86e0
Jeff Boulanger <fdsprod@gmail.com>
2019-08-06 15:44:27 -0700

    #11 Open URLs in users default browser when the URL is not embed

--- DCS.Alternative.Launcher/Plugins/Game/Views/GameView.xaml.cs
+++ DCS.Alternative.Launcher/Plugins/Game/Views/GameView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using CefSharp;
+using CefSharp.Handler;
 
 namespace DCS.Alternative.Launcher.Plugins.Game.Views
 {
@@ -7,6 +10,23 @@ namespace DCS.Alternative.Launcher.Plugins.Game.Views
         public GameView()
         {
             InitializeComponent();
+
+            Browser.RequestHandler = new RequestOverrideHandler();
+        }
+    }
+
+    public class RequestOverrideHandler : DefaultRequestHandler
+    {
+        public override bool OnBeforeBrowse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, bool userGesture, bool isRedirect)
+        {
+            if (request.Url.Contains("embed"))
+            {
+                return base.OnBeforeBrowse(browserControl, browser, frame, request, userGesture, isRedirect);
+            }
+
+            Process.Start(request.Url);
+            return true;
+
         }
     }
 }
