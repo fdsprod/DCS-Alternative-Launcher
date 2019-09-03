3931f13f3e2c1e9e54a4c4f6476e97df01d4e07c
Jeff Boulanger <fdsprod@gmail.com>
2019-09-03 15:13:47 -0700

    #40 Removed double click from main window

--- DCS.Alternative.Launcher/Windows/MainWindow.xaml.cs
+++ DCS.Alternative.Launcher/Windows/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace DCS.Alternative.Launcher.Windows
 {
@@ -7,17 +9,36 @@ namespace DCS.Alternative.Launcher.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int WM_NCLBUTTONDBLCLK = 0x00A3;
+
         public MainWindow()
         {
             InitializeComponent();
 
             AcrylicSource = BackgroundImage;
+            SourceInitialized += onSourceInitialized;
         }
 
         public static FrameworkElement AcrylicSource
         {
             get;
             private set;
+        }
+
+        private void onSourceInitialized(object sender, EventArgs e)
+        {
+            var source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            source.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_NCLBUTTONDBLCLK)
+            {
+                handled = true; //prevent double click from maximizing the window.
+            }
+
+            return IntPtr.Zero;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
