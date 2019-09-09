7f8bd2f00a1bde8038a9d69aff9567244128609d
Jeff Boulanger <fdsprod@gmail.com>
2019-09-08 21:03:01 -0700

    Fixed possible resource memory leak Fixed possible crash in retrieving a process working directory

--- DCS.Alternative.Launcher/Diagnostics/DcsProcessMonitor.cs
+++ DCS.Alternative.Launcher/Diagnostics/DcsProcessMonitor.cs
@@ -1,26 +1,104 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading;
 
 namespace DCS.Alternative.Launcher.Diagnostics
 {
     public class DcsProcessMonitor
     {
+        private const uint PathBufferSize = 512;
+        private readonly StringBuilder _pathBuffer = new StringBuilder((int) PathBufferSize);
+        private int _dcsProcessCountLastCheck;
+        private Timer _processMonitorTimer;
+
+        private DcsProcessMonitor()
+        {
+            // _processMonitorTimer = new Timer(OnTick, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+        }
+
         public static DcsProcessMonitor Instance
         {
             get;
         } = new DcsProcessMonitor();
 
-        private Timer _processMonitorTimer;
-        private int _dcsProcessCountLastCheck;
+        public event EventHandler DcsProcessExited;
 
-        public EventHandler DcsProcessExited;
-        public EventHandler DcsProcessStarted;
+        public event EventHandler DcsProcessStarted;
 
-        private DcsProcessMonitor()
+        private string GetExecutablePath(IntPtr hwnd)
         {
-            _processMonitorTimer = new Timer(OnTick, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+            if (hwnd == IntPtr.Zero)
+            {
+                return string.Empty;
+            } // not a valid window handle
+
+            // Get the process id
+            NativeMethods.GetWindowThreadProcessId(hwnd, out var processId);
+
+            // Try the GetModuleFileName method first since it's the fastest. 
+            // May return ACCESS_DENIED (due to VM_READ flag) if the process is not owned by the current user.
+            // Will fail if we are compiled as x86 and we're trying to open a 64 bit process...not allowed.
+            var hProcess = NativeMethods.OpenProcess(ProcessAccessFlags.QueryInformation | ProcessAccessFlags.Read, false, processId);
+
+            if (hProcess != IntPtr.Zero)
+            {
+                try
+                {
+                    if (NativeMethods.GetModuleFileNameEx(hProcess, IntPtr.Zero, _pathBuffer, PathBufferSize) > 0)
+                    {
+                        return _pathBuffer.ToString();
+                    }
+                }
+                finally
+                {
+                    NativeMethods.CloseHandle(hProcess);
+                }
+            }
+
+            hProcess = NativeMethods.OpenProcess(ProcessAccessFlags.QueryInformation, false, processId);
+
+            if (hProcess == IntPtr.Zero)
+            {
+                return string.Empty;
+            }
+
+            try
+            {
+                // Try this method for Vista or higher operating systems
+                var size = PathBufferSize;
+                if (Environment.OSVersion.Version.Major >= 6 && NativeMethods.QueryFullProcessImageName(hProcess, 0, _pathBuffer, ref size) && size > 0)
+                {
+                    return _pathBuffer.ToString();
+                }
+
+                // Try the GetProcessImageFileName method
+                if (NativeMethods.GetProcessImageFileName(hProcess, _pathBuffer, PathBufferSize) > 0)
+                {
+                    var path = _pathBuffer.ToString();
+
+                    foreach (var drive in Environment.GetLogicalDrives())
+                    {
+                        if (NativeMethods.QueryDosDevice(drive.TrimEnd('\\'), _pathBuffer, PathBufferSize) <= 0)
+                        {
+                            continue;
+                        }
+
+                        if (path.StartsWith(_pathBuffer.ToString()))
+                        {
+                            return drive + path.Remove(0, _pathBuffer.Length);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                NativeMethods.CloseHandle(hProcess);
+            }
+
+            return string.Empty;
         }
 
         private void OnTick(object state)
@@ -56,18 +134,36 @@ namespace DCS.Alternative.Launcher.Diagnostics
             return processes.Length > 0;
         }
 
+        private string[] GetPathsByProcessName(string processName)
+        {
+            var results = new List<string>();
+            var processes = Process.GetProcessesByName(processName);
+
+            foreach (var process in processes)
+            {
+                var path = GetExecutablePath(process.MainWindowHandle);
+
+                if (path != string.Empty)
+                {
+                    results.Add(path);
+                }
+            }
+
+            return results.ToArray();
+        }
+
         public bool IsDcsInstallUpdating(InstallLocation install)
         {
-            var processes = Process.GetProcessesByName("DCS_updater");
+            var paths = GetPathsByProcessName("DCS_updater");
 
-            return processes.Any(p => p.MainModule.FileName == install.ExePath);
+            return paths.Any(path => path == install.ExePath);
         }
 
         public bool IsDcsInstallRunning(InstallLocation install)
         {
-            var processes  = Process.GetProcessesByName("DCS");
+            var paths = GetPathsByProcessName("DCS");
 
-            return processes.Any(p => p.MainModule.FileName == install.ExePath);
+            return paths.Any(path => path == install.ExePath);
         }
     }
 }
---
+++ DCS.Alternative.Launcher/NativeMethods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCS.Alternative.Launcher
+{
+    public static class NativeMethods
+    {
+
+        [DllImport("psapi.dll")]
+        public static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, StringBuilder lpFilename, uint nSize);
+
+        [DllImport("psapi.dll")]
+        public static extern uint GetProcessImageFileName(IntPtr hProcess, StringBuilder lpImageFileName, uint nSize);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool QueryFullProcessImageName(IntPtr hProcess, uint dwFlags, StringBuilder lpExeName, ref uint lpdwSize);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint QueryDosDevice(string lpDeviceName, StringBuilder lpTargetPath, uint uuchMax);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr hObject);
+    }
+
+    [Flags]
+    public enum ProcessAccessFlags : uint
+    {
+        Read = 0x10, // PROCESS_VM_READ
+        QueryInformation = 0x400 // PROCESS_QUERY_INFORMATION
+    }
+}
